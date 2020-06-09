0143fd631c4a8eedd813b156411b9a89f68d5cd5
Wataru TOYA <watrty@gmail.com>
2020-06-09 22:18:16 +0900

    use

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const withSass = require("@zeit/next-sass")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = withSass({
+    webpack: (config, options) => {
+        config.optimization.minimizer = [];
+        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+
+        return config;
+    },
     cssModules: true,
-})
+});
