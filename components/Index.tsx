e1babcdb0acd59ffcca5d16981fcec95fa63f50a
Wataru TOYA <watrty@gmail.com>
2020-06-10 21:20:49 +0900

    fix

--- components/Index.tsx
+++ components/Index.tsx
@@ -1,25 +1,29 @@
-import styles from "./index.module.scss"
-import Head from "next/head"
+import styles from './index.module.scss';
+import Head from 'next/head';
 
 export default function Index(links: string[]) {
-    return (
-        <div className={styles.container}>
-        <Head>
-            <title>watta.org</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>watta.org</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main className={styles.mainColumn}>
-            <h1 className={styles.title}>
-            watta.org
-            </h1>
+      <main className={styles.mainColumn}>
+        <h1 className={styles.title}>watta.org</h1>
 
-            <div>
-                <ul>
-                    { links.map((link, index) => <li key={index}><a href={link} className={styles.listAnchor}>{link}</a></li>) }
-                </ul>
-            </div>
-        </main>
+        <div>
+          <ul>
+            {links.map((link, index) => (
+              <li key={index}>
+                <a href={link} className={styles.listAnchor}>
+                  {link}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
+      </main>
+    </div>
+  );
 }
