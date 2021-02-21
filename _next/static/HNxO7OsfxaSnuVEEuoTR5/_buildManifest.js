d81f1752642fc0fe2ac3a75c440bbda13aa2461a
kga <kga@users.noreply.github.com>
2021-02-21 00:58:40 +0000

    deploy: 1879fc4942af510264bf70c92c75012dd1b09af8

--- _next/static/HNxO7OsfxaSnuVEEuoTR5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F7ce33f654a1c19962f38.css","static\u002Fchunks\u002Fpages\u002Findex-20ab1c1e82b68e0168f4.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-880be71986e477882215.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
