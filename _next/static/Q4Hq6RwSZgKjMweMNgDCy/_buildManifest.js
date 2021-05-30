d5fe9f9e877f18cf7005f49cd327adbe83720561
kga <kga@users.noreply.github.com>
2021-05-30 10:59:46 +0000

    deploy: 21b3fa8455c305bb6d71246a2e4d8bd909f75784

--- _next/static/Q4Hq6RwSZgKjMweMNgDCy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F7ce33f654a1c19962f38.css","static\u002Fchunks\u002Fpages\u002Findex-cbe43424e0bd189b3858.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-880be71986e477882215.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/lUC73QDr8jAY-I86SNNAK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b37b57d8341bebf9bc84.css","static/chunks/pages/index-24d49032c0dd02b401eb.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
