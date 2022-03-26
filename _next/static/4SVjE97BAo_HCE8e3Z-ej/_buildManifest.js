c5c6da8bb477c65a9ee87501b38f7fb6cca06152
kga <kga@users.noreply.github.com>
2022-03-26 00:43:39 +0000

    deploy: 092c2d93ed58fc25466ce68674e196884f1644af

---
+++ _next/static/4SVjE97BAo_HCE8e3Z-ej/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/26bb5298076bcab3.css","static/chunks/pages/index-c0ea050d19919131.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
